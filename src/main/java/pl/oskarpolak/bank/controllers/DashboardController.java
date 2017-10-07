cf08f7670e3675e95811f888758c75a29486387c
Oskar <oskarbank@gmail.com>
2017-10-07 16:24:31 +0200

    asd

--- src/main/java/pl/oskarpolak/bank/controllers/DashboardController.java
+++ src/main/java/pl/oskarpolak/bank/controllers/DashboardController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.oskarpolak.bank.models.RequestForm;
 
 @Controller
@@ -13,6 +15,16 @@ public class DashboardController {
     public String dashboard(Model model){
         model.addAttribute("formRequest", new RequestForm());
         return "dashboardTemplate";
+    }
+
+    @PostMapping("/dashboard")
+    public String dashboard(@ModelAttribute("formRequest") RequestForm form, Model model){
+        model.addAttribute("infoCredit", canGetCredit(form));
+        return "dashboardTemplate";
+    }
+
+    private boolean canGetCredit(RequestForm form){
+        return (form.getMonthCash() - form.getMinusCash()) * 0.7 > form.getCreditCash()/form.getCreditTime();
     }
 
 }
